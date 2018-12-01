73fd343f1159288caa3241d8ce4d8b570f5c384a
DmitryKobzev <dmitriykobzev99@gmail.com>
2018-12-01 14:10:38 +0300

    Added Dijkstra algorithm

--- src/Strategy.py
+++ src/Strategy.py
@@ -1,3 +1,8 @@
+try:
+    import Queue as Q  # ver. < 3.0
+except ImportError:
+    import queue as Q
+
 
 class Strategy:
 
@@ -18,4 +23,24 @@ class Strategy:
 		#return self.moves
 		pass
 
-	
+	def Dijkstra(S, adjacencyLists):
+		priority_queue = Q.PriorityQueue()
+		priority_queue.put(S, 0)
+
+		came_from = {}
+		cost_so_far = {}
+		came_from[S] = S
+		cost_so_far[S] = 0
+
+		while not priority_queue.empty():
+			current = priority_queue.get()
+
+			for next in range(len(adjacencyLists[current])):
+				new_cost = cost_so_far[current] + adjacencyLists[current][next]
+				if next not in cost_so_far or new_cost < cost_so_far[next]:
+					cost_so_far[next] = new_cost
+					priority = new_cost
+					priority_queue.put(next, priority)
+					came_from[next] = current
+
+		return cost_so_far,came_from
