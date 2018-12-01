38e60cee7d5d2209ee59adf564e01e3e9ed38b3b
BossOfTheGym <vladimir.deikun1999@gmail.com>
2018-12-01 15:53:03 +0300

    Labels

--- src/Scene.py
+++ src/Scene.py
@@ -1,5 +1,5 @@
-from PyQt5.QtGui  import QPainter, QColor, QPen, QBrush, QPolygonF
-from PyQt5.QtCore import QPointF
+from PyQt5.QtGui  import QPainter, QColor, QPen, QFont, QBrush, QPolygonF, QTextOption
+from PyQt5.QtCore import QPointF, QRectF, Qt
 
 from SceneElements import Base, Town, Market, Storage
 from SceneElements import Road, Speed, Train
@@ -26,12 +26,24 @@ def initPolygon(sides):
 
 	return res
 
+def initTextOption():
+	option = QTextOption()
+	option.setAlignment(Qt.AlignCenter)
+
+	return option
+
+def initTextFont():
+	font = QFont('Helvetica')
+	
+	return font
+
+
 #my little glm lib
-def modelMat(size, pos):
+def modelMat(xSize, ySize, pos):
 	model = np.zeros((3, 3), np.float32)
 
-	model[0][0] = size / 2.0
-	model[1][1] = size / 2.0
+	model[0][0] = xSize / 2.0
+	model[1][1] = ySize / 2.0
 	model[2][2] = 1.0
 	model[0][2] = pos[0]
 	model[1][2] = pos[1]
@@ -73,10 +85,10 @@ class Scene:
 	RESOURCES = {
 		Base.BASE      : initPolygon(6)
 		, Base.TOWN    : initPolygon(4)
-		, Base.MARKET  : initPolygon(3)
+		, Base.MARKET  : initPolygon(5)
 		, Base.STORAGE : initPolygon(5)
 	}
-	ZERO_POINT = np.float32([0.0, 0.0, 1.0])
+	ZERO_POINT = np.float32([+0.0, +0.0, +1.0])
 
 	#sizes
 	BASE_SIZES = {
@@ -96,6 +108,7 @@ class Scene:
 	}
 	ROAD_COLOR  = QColor(  0,   0,   0)
 	TRAIN_COLOR = QColor(255,   0, 255)
+	LABEL_COLOR = QColor(255, 255, 255)
 
 	#special coefs
 	DEFAULT_ZOOM = 0.9
@@ -106,9 +119,20 @@ class Scene:
 	CAMERA_LIM_COEF = 0.1
 
 
+	BASE_LABEL_SIZE = 40
+	ROAD_LABEL_SIZE = 20
+
+	TEXT_X_SCALE = 1.00
+	TEXT_Y_SCALE = 0.40
+
+	TEXT_REL = 0.70
+
+	TEXT_OPTION = initTextOption()
+	TEXT_FONT   = initTextFont()
+
+
 	##init
 	def __init__(self, bases, roads, trains, window):
-
 		self.__initViewport(window)
 		self.__initCameraComp()
 
@@ -118,6 +142,10 @@ class Scene:
 		self.__initRoadsVectors(roads)
 
 		self.__initTrainsInfo(trains)
+
+		self.__initBasesLabelsInfo(bases)
+		self.__initRoadsLabelsInfo(roads)
+
 
 	def __initViewport(self, window):
 		self.w = window.size().width()
@@ -132,6 +160,7 @@ class Scene:
 		self.right =  self.w * (Scene.CAMERA_LIM_COEF + 1.0)
 		self.down  = -self.h * (Scene.CAMERA_LIM_COEF)
 		self.up    =  self.h * (Scene.CAMERA_LIM_COEF + 1.0)
+
 
 	def __initBasesInfo(self, bases, roads):
 		MAGIC_CONST = 3
@@ -157,7 +186,7 @@ class Scene:
 
 			pos = self.__toViewport(pos)
 
-			model = modelMat(size, pos)
+			model = modelMat(size, size, pos)
 			color = Scene.BASE_COLORS[baseType]
 			
 			self.basesInfo[idx] = RenderInfo(data, model, color)
@@ -169,7 +198,7 @@ class Scene:
 
 			idx1, idx2 = road.getAdjacentIdx()
 
-			data  = np.int32([idx1, idx2])
+			data  = np.int32([idx1, idx2, road.getLength()])
 			model = None
 			color = Scene.ROAD_COLOR
 
@@ -180,17 +209,14 @@ class Scene:
 
 		for idx, train in trains.items():
 			data  = Scene.RESOURCES[Base.TOWN]
-			model = modelMat(Scene.TRAIN_SIZE, Scene.ZERO_POINT)
+			model = modelMat(Scene.TRAIN_SIZE, Scene.TRAIN_SIZE, Scene.ZERO_POINT)
 			color = Scene.TRAIN_COLOR
 
 			self.trainsInfo[idx] = RenderInfo(data, model, color)
 
 			self.updateTrain(train)
-	
-	def __initLabelsInfo(self):
 
-		pass
-	
+
 	def __initRoadsVectors(self, roads):
 		self.roadsVecs = {}
 
@@ -200,10 +226,49 @@ class Scene:
 			mod1 = self.basesInfo[base1].model
 			mod2 = self.basesInfo[base2].model
 
-			vec = np.float32([mod2[0][2] - mod1[0][2], mod2[1][2] - mod1[1][2]])
+			vec = np.float32([mod2[0][2] - mod1[0][2], mod2[1][2] - mod1[1][2], 0.0])
 			vec /= road.length
 
 			self.roadsVecs[idx] = vec
+
+
+	def __initBasesLabelsInfo(self, bases):
+		self.basesLabelsInfo = {}
+
+		for idx, info in self.basesInfo.items():
+			baseType = bases[idx].getType()
+
+			size    = Scene.BASE_LABEL_SIZE
+			pos     = np.array(info.model[:, 2])
+			pos[1] -= size / 2
+
+			data  = Scene.RESOURCES[Base.TOWN]
+			model = modelMat(size * Scene.TEXT_X_SCALE, size * Scene.TEXT_Y_SCALE, pos)
+			color = Scene.LABEL_COLOR
+
+			self.basesLabelsInfo[idx] = RenderInfo(data, model, color)
+
+	def __initRoadsLabelsInfo(self, roads):
+		self.roadsLabelsInfo = {}
+
+		for idx, vec in self.roadsVecs.items():
+			length  = roads[idx].getLength()
+			base, _ = roads[idx].getAdjacentIdx()
+
+			if length % 2 == 0:
+				coef = length / 2 - 0.25
+			else:
+				coef = length / 2
+
+			pos  = np.array(self.basesInfo[base].model[:, 2])
+			pos += coef * vec
+
+			size  = Scene.ROAD_LABEL_SIZE
+			data  = Scene.RESOURCES[Base.TOWN]
+			model = modelMat(size, size, pos)
+			color = Scene.LABEL_COLOR
+
+			self.roadsLabelsInfo[idx] = RenderInfo(data, model, color)
 
 
 	##logic
@@ -246,6 +311,9 @@ class Scene:
 		self.__drawRoads(context)
 		self.__drawBases(context)
 		self.__drawTrains(context)
+		
+		self.__drawBaseLabels(context)
+		self.__drawRoadLabels(context)
 	
 	#my little openGL
 	def __transform(self, model, point):
@@ -273,9 +341,9 @@ class Scene:
 		for idx, info in self.basesInfo.items():
 			context.setBrush(QBrush(info.color))
 
-			self.__drawPolygon(context, info.data, info.model)
+			buffered = self.__bufferPolygon(context, info.data, info.model)
 
-			#context.drawText(info.model[0][2] - 8, info.model[1][2] - 15, str(idx))
+			context.drawPolygon(buffered)
 
 		context.setBrush(restore)
 
@@ -295,27 +363,61 @@ class Scene:
 		for idx, info in self.trainsInfo.items():
 			context.setBrush(QBrush(info.color))
 
-			self.__drawPolygon(context, info.data, info.model)
+			buffered = self.__bufferPolygon(context, info.data, info.model)
+
+			context.drawPolygon(buffered)
 
 		context.setBrush(restore)
 
-	def __drawLabels(self, context):
+	def __drawBaseLabels(self, context):
+		restore = context.brush()
 
-		pass
+		for idx, info in self.basesLabelsInfo.items():
+			context.setBrush(QBrush(info.color))
+
+			buffered = self.__bufferPolygon(context, info.data, info.model)
+			context.drawPolygon(buffered)
+
+			Scene.TEXT_FONT.setPointSize(
+				int(Scene.TEXT_REL * (buffered[0].y() - buffered[2].y()))
+			)
+
+			context.setFont(Scene.TEXT_FONT)
+			context.drawText(QRectF(buffered[2], buffered[0]), str(idx), Scene.TEXT_OPTION)
+
+		context.setBrush(restore)
+
+	def __drawRoadLabels(self, context):
+		restore = context.brush()
+
+		for idx, info in self.roadsLabelsInfo.items():
+			context.setBrush(QBrush(info.color))
+
+			buffered = self.__bufferPolygon(context, info.data, info.model)
+			context.drawPolygon(buffered)
+
+			Scene.TEXT_FONT.setPointSize(
+				int(Scene.TEXT_REL * (buffered[0].y() - buffered[2].y()))
+			)
+
+			length = self.roadsInfo[idx].data[2]
+			context.setFont(Scene.TEXT_FONT)
+			context.drawText(QRectF(buffered[2], buffered[0]), str(length), Scene.TEXT_OPTION)
+
+		context.setBrush(restore)
 
 	#primitives draw
-	def __drawPolygon(self, context, data, model):
+	def __bufferPolygon(self, context, data, model):
 		buffer = QPolygonF()
 
 		for point in data:
 			point = self.__transform(model, point)
-
 			buffer.append(QPointF(point[0], point[1]))
-			
-		context.drawPolygon(buffer)
+
+		return buffer
 
 	def __drawLine(self, context, info):
-		idx1, idx2 = info.data
+		idx1, idx2, _ = info.data
 
 		model1 = self.basesInfo[idx1].model
 		model2 = self.basesInfo[idx2].model
